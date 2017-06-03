739a506450bf7b9d87106c6495e8c94231d7cbe3
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-06-03 16:55:48 +0900

    Revert "add login"
    This reverts commit 758a76619f7aa5d4316c18b8297f1e87b29228b3.

--- www/js/app.js
+++ www/js/app.js
@@ -7,120 +7,72 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
 
-  .run(function ($ionicPlatform, $rootScope, $timeout) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-    $rootScope.authStatus = false;
-    //stateChange event
-    $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
-      $rootScope.authStatus = toState.authStatus;
-      if ($rootScope.authStatus) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-      }
-    });
-
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-      console.log("URL : " + toState.url);
-      if (toState.url == '/tab') {
-        console.log("match : " + toState.url);
-        $timeout(function () {
-          angular.element(document.querySelector('#leftMenu')).removeClass("hide");
-        }, 1000);
-      }
-    });
-
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
   })
 
-  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  // Each tab has its own nav history stack:
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
-
-      //--------------------------------------
-
-      .state('app.login', {
-        url: '/login',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/tab-signin.html'
-          }
-        },
-        authStatus: false
-      })
-      .state('app.signup', {
-        url: '/signup',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/tab-signup.html',
-          }
-        },
-        authStatus: false
-      })
-      //--------------------------------------
-      // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'DashCtrl'
-          }
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.graphs', {
+      url: '/graphs',
+      views: {
+        'tab-graphs': {
+          templateUrl: 'templates/tab-graphs.html',
+          controller: 'graphsCtrl'
         }
-      })
-      .state('tab.graphs', {
-        url: '/graphs',
-        views: {
-          'tab-graphs': {
-            templateUrl: 'templates/tab-graphs.html',
-            controller: 'graphsCtrl'
-          }
+      }
+    })
+  .state('tab.records', {
+      url: '/records',
+      views: {
+        'tab-records': {
+          templateUrl: 'templates/tab-records.html',
+          controller: 'recordsCtrl'
         }
-      })
-      .state('tab.records', {
-        url: '/records',
-        views: {
-          'tab-records': {
-            templateUrl: 'templates/tab-records.html',
-            controller: 'recordsCtrl'
-          }
-        }
-      })
+      }
+    })
 
 
-    $ionicConfigProvider.navBar.alignTitle('center')
+  $ionicConfigProvider.navBar.alignTitle('center')
 
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/login');
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
 
-  });
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,51 +1,5 @@
 angular.module('starter.controllers', [])
-  .controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout, $location, $ionicPopup) {
 
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
-
-    // Form data for the login modal
-    $scope.loginData = {};
-
-    //--------------------------------------------
-    $scope.login = function (user) {
-
-      if (typeof (user) == 'undefined') {
-        $scope.showAlert('Please fill username and password to proceed.');
-        return false;
-      }
-
-      if (user.username == 'demo@gmail.com' && user.password == 'demo') {
-        $location.path('/app/dashboard');
-      } else {
-        $scope.showAlert('Invalid username or password.');
-      }
-
-    };
-    //--------------------------------------------
-    $scope.logout = function () { $location.path('/app/login'); };
-    //--------------------------------------------
-    // An alert dialog
-    $scope.showAlert = function (msg) {
-      var alertPopup = $ionicPopup.alert({
-        title: 'Warning Message',
-        template: msg
-      });
-    };
-    //--------------------------------------------
-  })
-
-  .controller('ProfilesCtrl', function ($scope, Profiles) {
-    $scope.profiles = Profiles.all();
-  })
-
-  .controller('ProfileCtrl', function ($scope, $stateParams, Profiles) {
-    $scope.profile = Profiles.get($stateParams.profileId);
-  })
   .controller("DashCtrl", ["$scope", "$firebaseObject", function ($scope, $firebaseObject) {
     var ref = firebase.database().ref("realtime");
     var obj = $firebaseObject(ref);
@@ -356,7 +310,7 @@ angular.module('starter.controllers', [])
         angular.forEach(x, function (x) {
           x.id = cnt;
           x.drivingTime = Math.round(x.drivingTime / 3600) + "시간" + Math.round(x.drivingTime % 3600 / 60) + "분" + Math.round(x.drivingTime % 3600 % 60) + "초";
-          x.date = x.date.slice(0, 24);
+         x.date = x.date.slice(0,24);
           items.push(x);
           cnt++;
         })
